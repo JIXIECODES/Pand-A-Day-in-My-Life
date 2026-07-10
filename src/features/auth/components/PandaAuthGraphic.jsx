6706c972135c6d57999344a1679fa7c8b5961286
JIXIECODES <Tiffanywzy9@gmail.com>
2026-07-10 15:51:35 -0400

    Increase max width of PandaAuthGraphic component

--- src/features/auth/components/PandaAuthGraphic.jsx
+++ src/features/auth/components/PandaAuthGraphic.jsx
@@ -3,7 +3,7 @@ import authPanda from "../../../assets/auth/auth-panda.svg";
 export default function PandaAuthGraphic() {
   return (
     <div
-      className="mx-auto flex aspect-square w-full max-w-[650px] items-center justify-center"
+      className="mx-auto flex aspect-square w-full max-w-[1300px] items-center justify-center"
       role="img"
       aria-label="Panda holding bamboo"
     >
